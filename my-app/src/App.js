2c95e9210e5a4d048f2d6f0e651587640f4fbcfe
Bohdan Antonyk <antonuk1306@gmail.com>
2022-11-14 20:27:24 +0200

    commit1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <div className="container">
+                    <div className="wrapper-second">
+                        <header className='header'>
+                            <div className="header-left">
+                                <div className="wrapper-logo">
+                                    <img src="./img/logo.png" alt="Logo" className="logo"/>
+                                    <div className="header-name">
+                                        <h2>REACT SNEAKERS</h2>
+                                        <div className="header-text">
+                                            Магазин найкращих красовок
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="wrapper-right">
+                                <img src="./img/basket.svg" alt="Basket" className="basket"/>
+                                <div className="price">1200 грн</div>
+                                <img src="./img/love.svg" alt="toLove" className="love"/>
+                                <img src="/img/user.png" alt="user" className="user"/>
+                            </div>
+                        </header>
+                        <div className="wrapper-second-block">
+                            <h1>Всі красовки</h1>
+                            <input type="text" className="input"/>
+                        </div>
+                        <div className="block-card">
+                            <div className="card">
+                                <button className="block-love">
+                                    <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                                </button>
+                                <img src="./img/card.png" alt="card"/>
+                                <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                                <div className="wrapper-card">
+                                    <div className="wrapper-inner-card">
+                                        <p className='price'>Ціна :</p>
+                                        <div className="text-price">5000 грн</div>
+                                    </div>
+                                    <button className="ad-to-basket">+</button>
+                                </div>
+                            </div>
+                            <div className="card">
+                                <button className="block-love">
+                                    <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                                </button>
+                                <img src="./img/card.png" alt="card"/>
+                                <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                                <div className="wrapper-card">
+                                    <div className="wrapper-inner-card">
+                                        <p className='price'>Ціна :</p>
+                                        <div className="text-price">5000 грн</div>
+                                    </div>
+                                    <button className="ad-to-basket">+</button>
+                                </div>
+                            </div><div className="card">
+                            <button className="block-love">
+                                <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                            </button>
+                            <img src="./img/card.png" alt="card"/>
+                            <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                            <div className="wrapper-card">
+                                <div className="wrapper-inner-card">
+                                    <p className='price'>Ціна :</p>
+                                    <div className="text-price">5000 грн</div>
+                                </div>
+                                <button className="ad-to-basket">+</button>
+                            </div>
+                        </div><div className="card">
+                            <button className="block-love">
+                                <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                            </button>
+                            <img src="./img/card.png" alt="card"/>
+                            <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                            <div className="wrapper-card">
+                                <div className="wrapper-inner-card">
+                                    <p className='price'>Ціна :</p>
+                                    <div className="text-price">5000 грн</div>
+                                </div>
+                                <button className="ad-to-basket">+</button>
+                            </div>
+                        </div><div className="card">
+                            <button className="block-love">
+                                <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                            </button>
+                            <img src="./img/card.png" alt="card"/>
+                            <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                            <div className="wrapper-card">
+                                <div className="wrapper-inner-card">
+                                    <p className='price'>Ціна :</p>
+                                    <div className="text-price">5000 грн</div>
+                                </div>
+                                <button className="ad-to-basket">+</button>
+                            </div>
+                        </div><div className="card">
+                            <button className="block-love">
+                                <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                            </button>
+                            <img src="./img/card.png" alt="card"/>
+                            <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                            <div className="wrapper-card">
+                                <div className="wrapper-inner-card">
+                                    <p className='price'>Ціна :</p>
+                                    <div className="text-price">5000 грн</div>
+                                </div>
+                                <button className="ad-to-basket">+</button>
+                            </div>
+                        </div><div className="card">
+                            <button className="block-love">
+                                <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                            </button>
+                            <img src="./img/card.png" alt="card"/>
+                            <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                            <div className="wrapper-card">
+                                <div className="wrapper-inner-card">
+                                    <p className='price'>Ціна :</p>
+                                    <div className="text-price">5000 грн</div>
+                                </div>
+                                <button className="ad-to-basket">+</button>
+                            </div>
+                        </div><div className="card">
+                            <button className="block-love">
+                                <img src="./img/Vector (4).svg" alt="Love" className='loves-purple'/>
+                            </button>
+                            <img src="./img/card.png" alt="card"/>
+                            <p>Чоловічі Кросовки Nike Blazer Mid Suede</p>
+                            <div className="wrapper-card">
+                                <div className="wrapper-inner-card">
+                                    <p className='price'>Ціна :</p>
+                                    <div className="text-price">5000 грн</div>
+                                </div>
+                                <button className="ad-to-basket">+</button>
+                            </div>
+                        </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
